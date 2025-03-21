becf61f332a9fb6f2251aa21e7efaa95d0d1850b
mmbalbas <mmbalbasgolf@gmail.com>
2025-03-21 10:09:00 +0100

    Implementación del método getProductos en ProductoControllerRest para devolver la lista de productos

--- src/main/java/edu/tienda/core/controllers/ProductoControllerRest.java
+++ src/main/java/edu/tienda/core/controllers/ProductoControllerRest.java
@@ -1,11 +1,31 @@
 package edu.tienda.core.controllers;
 
+import edu.tienda.core.domain.Producto;
+import edu.tienda.core.services.ProductosServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 
 @RequestMapping("/productos")
 
 public class ProductoControllerRest {
+
+
+    //Se instancia la clase ProductosServiceImpl
+    private ProductosServiceImpl productosService = new ProductosServiceImpl();
+
+    @GetMapping
+    public ResponseEntity<?> getProductos() {
+
+        //Se obtiene la lista de productos
+        List<Producto> productos = productosService.getProductos();
+
+        //Se retorna la lista de productos
+        return ResponseEntity.ok(productos);
+    }
 }
