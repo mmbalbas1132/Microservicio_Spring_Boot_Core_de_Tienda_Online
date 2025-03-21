858907fb7671fb5b24d37a7d1993b18e1b102beb
mmbalbas <mmbalbasgolf@gmail.com>
2025-03-21 08:48:19 +0100

    excepción común y corriente sin ningún tipo de agregado Spring. Vamos a decorarla con dos anotaciones que nos permitirán aportar un comportamiento más dirigido en cuanto a su propósito y orientado al manejo de servicios REST.La primera anotación @ResponseBody indica que esta excepción, al lanzarse desde algún controlador, formará parte del cuerpo de respuesta de la petición. El segundo anotador indicará que al lanzarse esta excepcion, se reportara con un código de respuesta 404. Esto significa que Spring creará una “ResponseEntity” por nosotros al detectar el lanzamiento de esta excepción. Ahora cambiaremos nuestro código del controlador de manera sutil. Ahora simplemente lanzamos la excepcion “ResourceNotFoundException” de manera explícita en lugar de construir nosotros el código NOT FOUND con “ResponseEntity”. En este caso, Spring Boot generará ese código de respuesta por nosotros. Spring se encargará de generar el “ResponseEntity.notFound().build()” cuando detecte en tiempo de ejecución que se ha lanzado la excepcion “ResourceNotFoundException”. Este comportamiento, se logra gracias al decorador @ResponseStatus situado en la parte superior de la clase “ResourceNotFoundException”.

--- src/main/java/edu/tienda/core/controllers/ClienteResteController.java
+++ src/main/java/edu/tienda/core/controllers/ClienteResteController.java
@@ -2,9 +2,12 @@ package edu.tienda.core.controllers;
 
 
 import edu.tienda.core.domain.Cliente;
+import edu.tienda.core.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -29,28 +32,31 @@ public class ClienteResteController {
 
     //Obtener todos los clientes
     @GetMapping
-    public ResponseEntity <?> getClientes() {
+    public ResponseEntity<?> getClientes() {
         return ResponseEntity.ok(clientes);
     }
 
     //Obtener un cliente por su username
-//    @GetMapping("/clientes/{username}")
-//    public Cliente getCliente(@PathVariable String username) {
+//    @GetMapping("/{username}")
+//    public ResponseEntity <?>  getCliente(@PathVariable String username) {
 //        for (Cliente cliente : clientes) {
 //            if (cliente.getUsername().equalsIgnoreCase(username)) {
-//                return cliente;
+//                return ResponseEntity.ok(cliente);
 //            }
 //        }
-//        return null;
+//        throw new ResourceNotFoundException("Cliente no encontrado");
 //    }
 
-    //    Recodifico la oanterior como un stream y una expresión lambda
+    //Recodifico la oanterior como un stream y una expresión lambda
+    //En primer lugar se convierte la lista de clientes a tipo “stream” dándonos la posibilidad de ejecutar algunos métodos útiles. En este caso utilizamos “filter” que es el que necesitamos para realizar un filtrado por “userName”.  En segundo término se ejecuta el método “findFirst()” que retornará un opcional de cliente. El método “map” transformara el “Optional<Cliente>” en un “Optional<ResponseEntity<Cliente>>” y retornará este objeto directamente. Esto equivale a la siguiente codificación: “return ResponseEntity.ok(cliente)”. En el caso de que el opcional de cliente no contenga un objeto, se ejecutará la última línea que provocará la excepción. Luego será Spring el encargado de transformar la respuesta del servicio como NOT FOUND.
+
+
     @GetMapping("/{username}")
-    public ResponseEntity <?> getCliente(@PathVariable String username) {
-        return ResponseEntity.ok(clientes.stream()
-                .filter(cliente -> cliente.getUsername().equalsIgnoreCase(username))
-                .findFirst()
-                .orElseThrow());
+    public ResponseEntity<Cliente> getCliente(@PathVariable String username) {
+        return clientes.stream()
+                .filter(cliente -> cliente.getUsername().equals(username))
+                .findFirst().map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResourceNotFoundException("Cliente " + username + " no encontrado"));
     }
 
 
@@ -67,8 +73,8 @@ public class ClienteResteController {
 
     //Obtener un cliente por su nombre con stream y expresión lambda
     @GetMapping("/nombre/{nombre}")
-    public ResponseEntity <?> getClientePorNombre(@PathVariable String nombre) {
-        return ResponseEntity.ok (clientes.stream()
+    public ResponseEntity<?> getClientePorNombre(@PathVariable String nombre) {
+        return ResponseEntity.ok(clientes.stream()
                 .filter(cliente -> cliente.getNombre().equalsIgnoreCase(nombre))
                 .findFirst().orElseThrow());
 
@@ -97,8 +103,8 @@ public class ClienteResteController {
     //Para modificar un cliente, se debe enviar una petición PUT a la URL /clientes/{username} con el siguiente JSON en el cuerpo de la petición:
     //Para resover esto uso Postman
     @PutMapping
-    public ResponseEntity <?> modificarCliente(@RequestBody Cliente cliente) {
-       Cliente clienteEncontrado = clientes.stream()
+    public ResponseEntity<?> modificarCliente(@RequestBody Cliente cliente) {
+        Cliente clienteEncontrado = clientes.stream()
                 .filter(c -> c.getUsername().equalsIgnoreCase(cliente.getUsername()))
                 .findFirst()
                 .orElseThrow();
@@ -110,7 +116,7 @@ public class ClienteResteController {
     //Eliminar un cliente
     //Para eliminar un cliente, se debe enviar una petición DELETE a la URL /clientes/{username}
     @DeleteMapping("{username}")
-    public ResponseEntity  eliminarCliente(@PathVariable String username) {
+    public ResponseEntity eliminarCliente(@PathVariable String username) {
         Cliente clienteEncontrado = clientes.stream()
                 .filter(c -> c.getUsername().equalsIgnoreCase(username))
                 .findFirst()
